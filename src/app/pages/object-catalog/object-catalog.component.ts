8e36ec9fb275fedd3074501cda090eeec23d66bf
RicardoSMM <ricardowebdes@gmail.com>
2022-10-22 17:42:00 -0300

    Ajustes de html e css object catalog component

--- src/app/pages/object-catalog/object-catalog.component.ts
+++ src/app/pages/object-catalog/object-catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-object-catalog',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObjectCatalogComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(
+    private apiService: ApiService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+
+  getCatalogue() {
+    this.apiService.getItens().subscribe(() => {
+
+    })
   }
 
 }
