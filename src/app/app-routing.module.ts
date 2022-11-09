e731042b135df25c2a9fc69319a97e0bf772444d
RicardoSMM <ricardowebdes@gmail.com>
2022-11-09 16:43:29 -0300

    AuthGuard Service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,33 @@ import { LoginComponent } from './pages/login/login.component';
 import { OpenObjectComponent } from './pages/open-object/open-object.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'cadastrar', component: SignUpComponent },
-  { path: 'item/:id', component: OpenObjectComponent }
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'cadastrar',
+    component: SignUpComponent
+  },
+  {
+    path: 'item/:id',
+    component: OpenObjectComponent,
+    canActivate: [AuthGuardService]
+  }
 
 ];
 
