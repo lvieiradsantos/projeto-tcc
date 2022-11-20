32a0949af207495a3a4859ea251dad2210a1804e
lvieiradsantos <lvieiradev@gmail.com>
2022-11-20 02:56:40 -0300

    att da header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import { faGrip, faArrowRightToBracket, faArrowRightFromBracket, faCircleUser, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { UtilService } from 'src/app/services/util.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+
+  isLogged: boolean;
+
+  faGrip = faGrip; //catálogo
+  faArrowRightToBracket = faArrowRightToBracket; //entrar
+  faArrowRightFromBracket = faArrowRightFromBracket; //sair
+  faCircleUser = faCircleUser; //perfil
+  faUserPlus = faUserPlus; //cadastrar-se
+
+  constructor(
+    private utilService: UtilService
+  ) { }
+
+  ngOnInit(): void {
+    this.isLogged = !!this.utilService.getToken();
+    this.subscribeToIsLoggedSubject();
+  }
+
+  subscribeToIsLoggedSubject() {
+    this.utilService.isLoggedSubject
+      .subscribe(isLogged => {
+        this.isLogged = isLogged;
+      });
+  }
+
+  logout() {
+    this.utilService.logout();
+  }
+}
